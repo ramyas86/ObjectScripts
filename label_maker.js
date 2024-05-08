2495c04e247e137f90999072bcc4bbfe5eb34ae1
ramyas86 <ramya-s@pluralsight.com>
2024-05-08 15:21:38 +0530

    first commit

--- label_maker.js
+++ label_maker.js
@@ -2,14 +2,6 @@
 
 // Object to include name, address, city, state, zip
 
-
-function printContact($infoObj) {
-    console.log(`${$infoObj.name}`);
-    console.log(`${$infoObj.address}`);
-    console.log(`${$infoObj.city}, ${$infoObj.state} ${$infoObj.zip}`);
-};
-
-
 let myInfo = {
     name: "Ramya S",
     address: "BTM 1st Stage",
@@ -18,5 +10,13 @@ let myInfo = {
     zip: "560068"
 };
 
+// invoking the function
+function printContact(infoObj) {
+    console.log(`${infoObj.name}`);
+    console.log(`${infoObj.address}`);
+    console.log(`${infoObj.city}, ${infoObj.state} ${infoObj.zip}`);
+};
+
+//calling the function
 printContact(myInfo);
 
